c3361fdcee751a1bc9be07997b09db8ad29e6326
Andy Crockett <andy@crockett.io>
2017-07-12 10:50:51 -0500

    switched typing animation from CSS on the content property to pure JS for browser compatibility

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
     <div id="app">
-        <div class="title"><div class="cursor"></div></div>
+        <div class="title">
+            <div class="title-text" ref="text"></div>
+            <div class="cursor"></div>
+        </div>
         <div class="links">
             <a href="https://github.com/andycrockett">github</a>
             &nbsp;&middot;&nbsp;
@@ -12,5 +15,24 @@
 <script>
     export default {
         name: 'app',
+        data: () => ({
+            text: 'a n d y _ c r o c k e t t ;'.split(' ')
+        }),
+        mounted() {
+            setTimeout(() => {
+                this.addLetter(this.text, 0)
+            }, 2000)
+        },
+        methods: {
+            addLetter(letterArray, position) {
+                let textContainer       = this.$refs.text;
+                textContainer.innerText = textContainer.innerText + (letterArray[position])
+                if (letterArray[position + 1]) {
+                    setTimeout(() => {
+                        this.addLetter(letterArray, position + 1)
+                    }, Math.max(400 * Math.random(), 125))
+                }
+            }
+        }
     }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import styles from "./sass/main.scss"
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
     el: '#app',
     template: '<App/>',
