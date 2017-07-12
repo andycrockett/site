e66a3dc32db2b59e5f3452b1fd36d63fc1988e4b
vagrant <vagrant@homestead>
2017-07-12 10:24:18 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="app">
+        <div class="title"><div class="cursor"></div></div>
+        <div class="links">
+            <a href="https://github.com/andycrockett">github</a>
+            &nbsp;&middot;&nbsp;
+            <a href="https://www.linkedin.com/in/andy-crockett-97695a91/">LinkedIn</a>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'app',
+    }
+</script>
